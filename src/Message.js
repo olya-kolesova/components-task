9d3b23410f63f4477587d0c87d3c458e39115c38
Olga Kolesova <olka.kolesova@gmail.com>
2021-11-25 22:04:51 +0000

    used props

---
+++ src/Message.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Message = props => {
+  return (
+    <div className="ui message">
+      <div className="header">
+        {props.header}
+      </div>
+      <p>
+        {props.text}
+      </p>
+    </div>
+  );
+};
+
+export default Message;
